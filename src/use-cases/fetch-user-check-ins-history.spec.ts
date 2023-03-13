1baa1e19c60d4930b6df1cbd2c5877a4ec658aea
K4geNo <micaraujo71@gmail.com>
2023-03-13 15:48:34 -0300

    Caso de uso de histórico

---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,67 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { FetchUserCheckInsHistoryUseCase } from './fetch-user-check-ins-history'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: FetchUserCheckInsHistoryUseCase
+
+describe('Fetch User Check in History Use Case', () => {
+    beforeEach(async () => {
+        checkInsRepository = new InMemoryCheckInsRepository()
+        sut = new FetchUserCheckInsHistoryUseCase(checkInsRepository)
+    })
+
+    it('should be able to fetch check in history', async () => {
+        await checkInsRepository.create({
+            gym_id: 'gym-id',
+            user_id: 'user-id',
+        })
+
+        await checkInsRepository.create({
+            gym_id: 'gym-id-2',
+            user_id: 'user-id',
+        })
+
+        const { checkIns } = await sut.execute({
+            userId: 'user-id',
+            page: 1,
+        })
+
+        expect(checkIns).toHaveLength(2)
+        expect(checkIns).toEqual([
+            expect.objectContaining({
+                gym_id: 'gym-id',
+            }),
+
+            expect.objectContaining({
+                gym_id: 'gym-id-2',
+            }),
+        ])
+    })
+
+    it('should be able to fetch paginated check in history', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await checkInsRepository.create({
+                gym_id: `gym-id-${i}`,
+                user_id: 'user-id',
+            })
+        }
+
+        const { checkIns } = await sut.execute({
+            userId: 'user-id',
+            page: 2,
+        })
+
+        expect(checkIns).toHaveLength(2)
+        expect(checkIns).toEqual([
+            expect.objectContaining({
+                gym_id: 'gym-id-21',
+            }),
+
+            expect.objectContaining({
+                gym_id: 'gym-id-22',
+            }),
+        ])
+    })
+})
