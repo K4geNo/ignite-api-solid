015d12cd1a122afcc2e8d12b68e87e57329f3ffb
K4geNo <micaraujo71@gmail.com>
2023-03-13 23:19:27 -0300

    Caso de uso de busca de academias

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,10 +1,17 @@
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 import { Gym, Prisma } from '@prisma/client'
 
-import { GymsRepository } from '../gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements GymsRepository {
     public items: Gym[] = []
+
+    async searchMany(query: string, page: number) {
+        return this.items
+            .filter((item) => item.title.includes(query))
+            .slice((page - 1) * 20, page * 20)
+    }
 
     async findById(id: string) {
         const gym = this.items.find((item) => item.id === id)
@@ -14,6 +21,23 @@ export class InMemoryGymsRepository implements GymsRepository {
         }
 
         return gym
+    }
+
+    async findManyNearby(params: FindManyNearbyParams) {
+        return this.items.filter((item) => {
+            const distance = getDistanceBetweenCoordinates(
+                {
+                    latitude: params.latitude,
+                    longitude: params.longitude,
+                },
+                {
+                    latitude: Number(item.latitude),
+                    longitude: Number(item.longitude),
+                },
+            )
+
+            return distance < 10
+        })
     }
 
     async create(data: Prisma.GymCreateInput) {
