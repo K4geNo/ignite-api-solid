25f8acdb04b40e18d0184b3a8eccc061ab5d68db
K4geNo <micaraujo71@gmail.com>
2023-03-14 12:27:17 -0300

    Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { authenticateController } from './controllers/authenticate'
 import { profileController } from './controllers/profile'
 import { registerController } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', registerController)
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
     app.post('/sessions', authenticateController)
 
     /** Authenticated */
-    app.get('/me', profileController)
+    app.get('/me', { onRequest: [verifyJWT] }, profileController)
 }
