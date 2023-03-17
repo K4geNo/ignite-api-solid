3bbc922bce233dcf3d38aaaf9b60c1e6f481f3ef
K4geNo <micaraujo71@gmail.com>
2023-03-17 11:08:11 -0300

    Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-    expect(1).toBe(1)
+import { app } from '@/app'
+import request from 'supertest'
+
+describe('Register Controller (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to register a user', async () => {
+        const response = await request(app.server).post('/users').send({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        })
+
+        expect(response.statusCode).toEqual(201)
+    })
 })
